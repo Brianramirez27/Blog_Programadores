c1e39a75fb811d523d01dfca261d336687178910
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-07-10 13:01:40 -0500

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,6 +1,6 @@
 <?php 
 // se crear las costante para que  usarlas en la pgina y que no hallan errores y sean mas facil utilizarlas
-define("base_url","http://localhost/blog_programadores/");
+define("base_url","https://github.com/Brianramirez27/Blog_Programadores/tree/main/");
 define("controller_defaul","controllerArticulo");
 define("action_defaul","indexUltimosArticulos");
 ?>
