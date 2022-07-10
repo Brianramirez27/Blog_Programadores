9e81412a9a8375cb2fde83c8878bf54fbf8e943e
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2022-07-10 13:04:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include_once("<?=base_url?>/views/layout/header.php") ?>
-<?php include_once("views/layout/menu.php") ?>
-<?php include_once("views/layout/aside.php") ?>
-<?php include_once("views/layout/usuario/loginRegistro.php") ?> 
+<?php include_once("<?=base_url?>/views/layout/menu.php") ?>
+<?php include_once("<?=base_url?>/views/layout/aside.php") ?>
+<?php include_once("<?=base_url?>/views/layout/usuario/loginRegistro.php") ?> 
 
 
 <div class="container">
